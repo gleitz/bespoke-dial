fe0d1f9b0ac15e3a3f068522ecf81a753d69f373
Benjamin Gleitzman <gleitz@mit.edu>
2012-12-09 10:24:46 -0800

    fix for indentation of telephone

--- telephone.ino
+++ telephone.ino
@@ -39,32 +39,32 @@ void loop()
     int reading = digitalRead(in);
     //Serial.println(reading);
     if ((millis() - lastStateChangeTime) > dialHasFinishedRotatingAfterMs) {
-	// the dial isn't being dialed, or has just finished being dialed.
-	if (needToPrint) {
-	    // if it's only just finished being dialed, we need to send the number down the serial
-	    // line and reset the count. We mod the count by 10 because '0' will send 10 pulses.
-	    Serial.println(count % 10);
+        // the dial isn't being dialed, or has just finished being dialed.
+        if (needToPrint) {
+            // if it's only just finished being dialed, we need to send the number down the serial
+            // line and reset the count. We mod the count by 10 because '0' will send 10 pulses.
+            Serial.println(count % 10);
             MP3player.playTrack(count % 11);
-	    needToPrint = 0;
-	    count = 0;
-	    cleared = 0;
-	}
+            needToPrint = 0;
+            count = 0;
+            cleared = 0;
+        }
     }
 
     if (reading != lastState) {
-	lastStateChangeTime = millis();
+        lastStateChangeTime = millis();
     }
     if ((millis() - lastStateChangeTime) > debounceDelay) {
-	// debounce - this happens once it's stablized
-	if (reading != trueState) {
-	    // this means that the switch has either just gone from closed->open or vice versa.
-	    trueState = reading;
-	    if (trueState == HIGH) {
-		// increment the count of pulses if it's gone high.
-		count++;
-		needToPrint = 1; // we'll need to print this number (once the dial has finished rotating)
-	    }
-	}
+        // debounce - this happens once it's stablized
+        if (reading != trueState) {
+            // this means that the switch has either just gone from closed->open or vice versa.
+            trueState = reading;
+            if (trueState == HIGH) {
+                // increment the count of pulses if it's gone high.
+                count++;
+                needToPrint = 1; // we'll need to print this number (once the dial has finished rotating)
+            }
+        }
     }
     lastState = reading;
 }
